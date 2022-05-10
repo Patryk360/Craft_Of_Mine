2600c7ed1ff068bf2268d3d6cec37b9f3d14dab6
Patryk360 <nexigamingyt@gmail.com>
2022-05-10 17:47:17 +0200

    0.0.1

---
+++ main.js
@@ -0,0 +1,8 @@
+const { WebSocketServer } = require("ws");
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+    ws.on("message", (data) => {
+      console.log(data.toString());
+    });
+});
